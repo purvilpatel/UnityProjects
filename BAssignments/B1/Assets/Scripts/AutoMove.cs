014be07a97afffe33b613398b76e08e4e24e6cb3
Parth <parth.shah124@gmail.com>
2015-10-18 20:37:58 -0400

    everything working

---
+++ BAssignments/B1/Assets/Scripts/AutoMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoMove : MonoBehaviour {
+    int move = 1;
+	// Use this for initialization
+	void Start () {
+        move = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = 5 * Time.deltaTime;
+        if (move == 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(10, 0.8f, -4), step);
+            if (transform.position == new Vector3(10, 0.8f, -4))
+            {
+                move = -1;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(14, 0.8f, -4), step);
+            if (transform.position == new Vector3(14, 0.8f, -4))
+            {
+                move = 1;
+            }
+        }
+	}
+}
